41a81386451d2cc310e8554ae9975561cb1fe42f
Sssevil <zhunushova29@gmail.com>
2024-03-16 00:46:49 +0600

    dz3

--- project/js/lesson.js
+++ project/js/lesson.js
@@ -15,3 +15,75 @@ phoneButton.addEventListener('click',()=> {
         phoneSpan.style.color='red'
     }
 })
+
+//TAB SLIDER
+
+const tabContents = document.querySelectorAll('.tab_content_block')
+const tabs = document.querySelectorAll('.tab_content_item')
+const tabsParent =document.querySelector('.tab_content_items')
+
+
+const hideContent = () => {
+    tabContents.forEach((content)=>{
+        content.style.display='none'
+    })
+
+    tabs.forEach((tab)=> {
+        tab.classList.remove('tab_content_item_active')
+    })
+}
+
+const showTabContent=(index=0)=>{
+    tabContents[index].style.display='block'
+    tabs[index].classList.add('tab_content_item_active')
+}
+
+hideContent()
+showTabContent()
+
+tabsParent.onclick=(event)=>{
+    if(event.target.classList.contains('tab_content_item')){
+        tabs.forEach((tab,tabIndex)=>{
+            if(event.target===tab){
+                hideContent()
+                showTabContent(tabIndex)
+            }
+        })
+    }
+}
+
+let interval
+
+const autoTabs =(i=0)=> {
+    interval = setInterval(()=>{
+        i++
+        if( i > tabContents.length-1){
+            i=0
+        }
+        hideContent()
+        showTabContent(i)
+    },3000)
+}
+autoTabs()
+
+tabs.forEach((tab)=>{
+    tab.onclick=(e)=>{
+        clearInterval(interval)
+        autoTabs(Array.from(tabs).indexOf(e.target))
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ project/js/modal.js
@@ -0,0 +1,42 @@
+//MODAL
+
+const modal = document.querySelector('.modal')
+const modalTrigger = document.querySelector('#btn-get')
+const modalCloseButton = document.querySelector('.modal_close')
+const openModal = ()=> {
+    modal.style.display='block'
+    document.body.style.overflow='hidden'
+}
+
+const closeModal =()=>{
+    modal.style.display='none'
+    document.body.style.overflow=''
+}
+modalTrigger. onclick=()=>{
+    openModal()
+}
+
+modalCloseButton.onclick=()=>{
+    closeModal()
+}
+
+modal.onclick = (event)=>{
+    if(event.target===modal){
+        closeModal()
+    }
+}
+
+const lastScroll = ()=>{
+    return window.innerHeight + window.scrollY >= document.documentElement.scrollHeight
+}
+
+const showModalScroll = ()=>{
+    if(lastScroll()){
+        openModal()
+        window.removeEventListener("scroll", showModalScroll)
+    }
+}
+
+window.addEventListener("scroll", showModalScroll)
+
+setTimeout(()=> openModal(),10000)
