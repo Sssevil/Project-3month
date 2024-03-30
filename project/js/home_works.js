c7614f493c73119e4dccaf5f66b7abacecb393d8
Sssevil <zhunushova29@gmail.com>
2024-03-30 22:32:51 +0600

    last

--- project/js/home_works.js
+++ project/js/home_works.js
@@ -8,7 +8,7 @@ const gmailBlock = document.querySelector('.form_gmail')
 const regExp=/^[a-zA-Z0-9._-]{3,}@gmail\.com$/
 
 gmailButton.addEventListener('click',()=> {
-    if (regExp.test(gmailInput.value.trim())){
+    if (regExp.test(gmailInput.value.trim()) && /[a-zA-Z]/.test(gmailInput.value.trim().split('@')[0])){
         gmailResult.innerHTML='Почта валидна'
         gmailResult.style.color='green'
         gmailBlock.style.boxShadow='rgba(121, 210, 118, 0.25) 0 50px 100px -20px, rgba(57, 238, 43, 0.87) 0 30px 60px -30px, rgba(25, 64, 10, 0.35) 0 -2px 6px 0 inset'
